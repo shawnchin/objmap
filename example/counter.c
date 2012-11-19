f74acd6d0083401956a9c77f66d15eb1964a3071
Shawn Chin <shawnchin@gmail.com>
2012-11-19 17:40:15 +0000

    Made example code more compact. Removed accidentally added bin

--- example/counter.c
+++ example/counter.c
@@ -10,6 +10,12 @@ struct _counter {
 /*! \brief Map to store counters */
 static ObjectMap *map = NULL;
 
+/*! \brief utility func to return mapped obj given a key */
+inline static struct _counter* unmap(counter key) {
+  assert(map != NULL); /* make sure map initialised */
+  return (struct _counter*)objmap_get(map, key);
+}
+
 /* create the map to store counter objects */
 void counter_manager_init(void) {
   if (map != NULL) return; /* already initialised */
@@ -43,48 +49,34 @@ counter counter_new(void) {
   return c;
 }
 
-/* retrieve object and set value back to 0 */
-void counter_reset(counter c) {
-  struct _counter *obj;
-  assert(map != NULL); /* make sure map initialised */
-  
-  obj = (struct _counter*)objmap_get(map, c);
-  if (obj != NULL) obj->value = 0;
-}
-
-/* retrieve object and increment value. Return new value. */
-unsigned int counter_increment(counter c) {
-  struct _counter *obj;
-  assert(map != NULL); /* make sure map initialised */
-  
-  /* retrieve object */
-  obj = (struct _counter*)objmap_get(map, c);
-  if (obj == NULL) return 0;  /* invalid key */
-  
-  obj->value++;
-  return obj->value;
-}
-
-/* retrieve object and return current value. */
-unsigned int counter_peek(counter c) {
-  struct _counter *obj;
-  assert(map != NULL); /* make sure map initialised */
-  
-  /* retrieve object */
-  obj = (struct _counter*)objmap_get(map, c);
-  if (obj == NULL) return 0; /* invalid key */
-  
-  return obj->value;
-}
-
 /* remove from map and deallocate object */
 void counter_delete(counter *c_ptr) {
   struct _counter *obj;
   counter c = *c_ptr;   /* get handle */
-  assert(map != NULL); /* make sure map initialised */
-  
   *c_ptr = COUNTER_NULL; /* set user's handle to NULL value */
   
+  assert(map != NULL); /* make sure map initialised */
   obj = objmap_pop(map, c); /* retrieve and remove from map */
+  
   if (obj != NULL) free(obj);
 }
+
+/* retrieve object and set value back to 0 */
+void counter_reset(counter c) {
+  struct _counter *obj = unmap(c);
+  if (obj != NULL) obj->value = 0;
+}
+
+/* retrieve object and increment value. Return new value.
+ * If counter is invalid, 0 is returned */
+unsigned int counter_increment(counter c) {
+  struct _counter *obj = unmap(c);
+  return (obj == NULL) ? 0 : ++obj->value;
+}
+
+/* retrieve object and return current value, or 0 if invalid counter*/
+unsigned int counter_peek(counter c) {
+  struct _counter *obj = unmap(c);
+  return (obj == NULL) ? 0 : obj->value; 
+}
+
